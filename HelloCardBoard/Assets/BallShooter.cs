39df1feed4d7595bcb1f65221328955667d55ed3
sayali <sayalisonawane27@gmail.com>
2018-09-07 10:21:13 -0600

    assignment 1

---
+++ HelloCardBoard/Assets/BallShooter.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody))]
+public class BallShooter : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
+
+public class ExampleClass : MonoBehaviour
+{
+    void OnCollisionEnter(Collision other)
+    {
+        print("Points colliding: " + other.contacts.Length);
+        print("First point that collided: " + other.contacts[0].point);
+    }
+}
---
+++ HelloCardBoard/Assets/bouncing.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class bouncing : MonoBehaviour {
+
+    public float moveSpeed = 10f;
+
+    private Rigidbody rbody;
+
+	// Use this for initialization
+	void Start () {
+        rbody = GetComponent<Rigidbody>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        //float inputX = Input.GetAxis("Horizontal");
+        //float inputY = Input.GetAxis("Jump");
+        //float inputZ = Input.GetAxis("Vertical");
+
+        //float moveX = inputX * moveSpeed * Time.deltaTime;
+        //float moveY = inputY * moveSpeed * Time.deltaTime;
+        //float moveZ = inputZ * moveSpeed * Time.deltaTime;
+
+        //transform.Translate(moveX, 0f, moveZ);
+        transform.Translate(Input.acceleration.x, 0, -Input.acceleration.z);
+
+        //rbody.AddForce(moveX, moveY, moveZ);
+
+    }
+}
